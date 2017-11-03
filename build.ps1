e338096bf9c551f08b48bbd27272eb96b0f9141b
Dmitry Titenko <d.titenko@evision-software.com>
2017-11-03 17:25:52 +0100

    Update build script

--- build.ps1
+++ build.ps1
@@ -1,3 +1,4 @@
 Push-Location .\test-data
+Get-ChildItem * -Exclude *.zip -Recurse | Remove-Item -Recurse
 Get-ChildItem -Filter *.zip | ForEach-Object { Expand-Archive $_.Name -DestinationPath .\ }
 Pop-Location
