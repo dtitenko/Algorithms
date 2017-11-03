01aff28b53f2e4172c1be0719da00c1d8a30d785
Dmitry Titenko <d.titenko@evision-software.com>
2017-11-03 17:45:36 +0100

    Energy calculation

--- src/SeamCarver.java
+++ src/SeamCarver.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+
 public class SeamCarver {
     private Picture _picture;
 
@@ -26,14 +28,34 @@ public class SeamCarver {
         if (x < 0 || x >= _picture.width() || y < 0 || y >= _picture.height()) {
             throw new java.lang.IllegalArgumentException();
         }
+
+        if (x == 0 || y == 0 || x == _picture.width() - 1 || y == _picture.height() - 1) {
+        	return 1000;
+        }
+
+        Color colorX1 = _picture.get(x + 1, y);
+        Color colorX2 = _picture.get(x - 1, y);
+        Color colorY1 = _picture.get(x, y + 1);
+        Color colorY2 = _picture.get(x, y - 1);
+
+        return Math.sqrt(
+            Math.pow(colorX1.getRed() - colorX2.getRed(), 2) +
+            Math.pow(colorX1.getGreen() - colorX2.getGreen(), 2) +
+            Math.pow(colorX1.getBlue() - colorX2.getBlue(), 2) +
+            Math.pow(colorY1.getRed() - colorY2.getRed(), 2) +
+            Math.pow(colorY1.getGreen() - colorY2.getGreen(), 2) +
+            Math.pow(colorY1.getBlue() - colorY2.getBlue(), 2)
+        );
     }
 
     // sequence of indices for horizontal seam
     public int[] findHorizontalSeam() {
+        throw new UnsupportedOperationException();
     }
 
     // sequence of indices for vertical seam
     public int[] findVerticalSeam() {
+        throw new UnsupportedOperationException();
     }
 
     // remove horizontal seam from current picture
@@ -42,5 +64,20 @@ public class SeamCarver {
 
     // remove vertical seam from current picture
     public void removeVerticalSeam(int[] seam) {
+    }
+
+    public static void main(String[] args) {
+        Picture picture = new Picture(args[0]);
+        StdOut.printf("image is %d pixels wide by %d pixels high.\n", picture.width(), picture.height());
+
+        SeamCarver sc = new SeamCarver(picture);
+
+        StdOut.printf("Printing energy calculated for each pixel.\n");
+
+        for (int row = 0; row < sc.height(); row++) {
+            for (int col = 0; col < sc.width(); col++)
+                StdOut.printf("%7.2f ", sc.energy(col, row));
+            StdOut.println();
+        }
     }
 }
