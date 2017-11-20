d159cf7385ac3577ad6e89014f8f4886c3439062
Dmitry Titenko <d.titenko@evision-software.com>
2017-11-20 12:34:27 +0100

    Fill ST dictionary

--- src/BoggleSolver.java
+++ src/BoggleSolver.java
@@ -1,8 +1,15 @@
+import edu.princeton.cs.algs4.TrieST;
+
 public class BoggleSolver {
-    // Initializes the data structure using the given array of strings as the dictionary.
+    private TrieST<Integer> _dictionary = new TrieST<Integer>();
+
+	// Initializes the data structure using the given array of strings as the dictionary.
     // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
     public BoggleSolver(String[] dictionary) {
-
+    	for (int i = 0; i < dictionary.length; i++) {
+            String key = dictionary[i];
+            _dictionary.put(key, i);
+        }
     }
 
     // Returns the set of all valid words in the given Boggle board, as an Iterable.
@@ -13,7 +20,7 @@ public class BoggleSolver {
     // Returns the score of the given word if it is in the dictionary, zero otherwise.
     // (You can assume the word contains only the uppercase letters A through Z.)
     public int scoreOf(String word) {
-        if (word == null || word.length() < 3)
+        if (word == null || word.length() < 3 || !_dictionary.contains(word))
             return 0;
         if (word.length() < 5)
             return 1;
