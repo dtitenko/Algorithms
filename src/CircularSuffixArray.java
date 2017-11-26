3b84571f5b447ca1a7d6d782f9791dd584e057bf
Dmitry Titenko <d.titenko@evision-software.com>
2017-11-26 16:41:32 +0100

    BurrowsWheeler scaffolding

---
+++ src/CircularSuffixArray.java
@@ -0,0 +1,19 @@
+public class CircularSuffixArray {
+    public CircularSuffixArray(String s) {
+        // circular suffix array of s
+    }
+
+    public int length() {
+        // length of s
+        throw new UnsupportedOperationException();
+    }
+
+    public int index(int i) {
+        // returns index of ith sorted suffix
+        throw new UnsupportedOperationException();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/MoveToFront.java
@@ -0,0 +1,17 @@
+public class MoveToFront {
+    // apply move-to-front encoding, reading from standard input and writing to standard output
+    public static void encode() {
+        throw new UnsupportedOperationException();
+    }
+
+    // apply move-to-front decoding, reading from standard input and writing to standard output
+    public static void decode() {
+        throw new UnsupportedOperationException();
+    }
+
+    // if args[0] is '-', apply move-to-front encoding
+    // if args[0] is '+', apply move-to-front decoding
+    public static void main(String[] args) {
+        throw new UnsupportedOperationException();
+    }
+}
