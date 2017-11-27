9cc1924e08ed75d717c9249b54401480ea1fab4f
Dmitry Titenko <d.titenko@evision-software.com>
2017-11-27 22:04:21 +0100

    Burrows-Wheeler - Move-to-front

--- src/MoveToFront.java
+++ src/MoveToFront.java
@@ -1,17 +1,53 @@
 public class MoveToFront {
-    // apply move-to-front encoding, reading from standard input and writing to standard output
+    private static final int R = 256;
+
+    // apply move-to-front encoding, reading from standard input and writing to
+    // standard output
     public static void encode() {
-        throw new UnsupportedOperationException();
+        char[] chars = radixList();
+        char count, ch, tmpin, tmpout;
+        while (!BinaryStdIn.isEmpty()) {
+            ch = BinaryStdIn.readChar();
+            for (count = 0, tmpout = chars[0]; ch != chars[count]; count++) {
+                tmpin = chars[count];
+                chars[count] = tmpout;
+                tmpout = tmpin;
+            }
+            chars[count] = tmpout;
+            BinaryStdOut.write(count);
+            chars[0] = ch;
+        }
+        BinaryStdOut.close();
     }
 
-    // apply move-to-front decoding, reading from standard input and writing to standard output
+    // apply move-to-front decoding, reading from standard input and writing to
+    // standard output
     public static void decode() {
-        throw new UnsupportedOperationException();
+        char[] chars = radixList();
+        char count, ch;
+        while (!BinaryStdIn.isEmpty()) {
+            count = BinaryStdIn.readChar();
+            for (ch = chars[count]; count > 0; chars[count] = chars[--count])
+                ;
+            chars[count] = ch;
+            BinaryStdOut.write(ch);
+        }
+        BinaryStdOut.close();
+    }
+
+    private static char[] radixList() {
+        char[] rl = new char[R];
+        for (char i = 0; i < R; rl[i] = i++);
+        return rl;
     }
 
     // if args[0] is '-', apply move-to-front encoding
     // if args[0] is '+', apply move-to-front decoding
     public static void main(String[] args) {
-        throw new UnsupportedOperationException();
+        if (args[0] == "-") {
+            encode();
+        } else if (args[0] == "+") {
+            decode();
+        }
     }
 }
